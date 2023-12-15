c91f46c138d3bc5e51cd0eefb163cb889d397ac8
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2023-12-15 15:05:25 +0200

    advent of code 2023 day two completed

--- day-2/index.ts
+++ day-2/index.ts
@@ -18,6 +18,19 @@ function getGameNumbers(game: string): number{
   return parseInt(gameNumber);
 }
 
+function getGameSets(game: string): string[][]{
+  const gameSplit = game.split(":");
+  const setsArr = gameSplit[1].split(";");
+  const cleanSetArr: string[][] = [];
+  
+  setsArr.forEach((set)=>{
+    const withoutEmptyStrings = set.split(' ').filter(el => !!el).join(" ").split(', ');
+    cleanSetArr.push(withoutEmptyStrings)
+  })
+  
+  return cleanSetArr;
+}
+
 function getGameAnswer(game: string): boolean{
   let res = true;
   const gameSplit = game.split(":");
@@ -50,15 +63,66 @@ function getGameAnswer(game: string): boolean{
 
   return res;
 }
+// get the minimum required balls in a color needed to play the game
+function getMinimumBallNum(sets: string[][]): number[]{
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+  // returns in rgb format
+  sets.forEach((set)=>{
+    set.forEach(ball => {
+      const num = ball.split(" ")[0];
+      const color = ball.split(" ")[1];
+      if(color === 'red'){
+        if(red < parseInt(num)){
+          red = parseInt(num);
+        }
+      }
+      if(color === 'green'){
+        if(green < parseInt(num)){
+          green = parseInt(num);
+        }
+      }
+      if(color === 'blue'){
+        if(blue < parseInt(num)){
+          blue = parseInt(num);
+        }
+      }
+    })
+  })
+  return [ red, green, blue]
+}
 
-async function main(){
-  const path = './input.txt'
-  const file = Bun.file(path);
-  
-  const text = await file.text();
-  
-  const gamesArr = text.split('\n');
+// get the power of all minimum ball numbers ie: red * green * blue
+function getPowerOfBalls(minBallRGB: number[]): number{
+  let result = 1;
+  for (let i = 0; i < minBallRGB.length; i++) {
+    const num = minBallRGB[i];
+    // const nextNum = minBallRGB[i + 1] === undefined ? 1 : minBallRGB[i + 1]
 
+    result = result * num
+  }
+  return result;
+}
+
+function getSumOfArrNum(arr: number[]):number{
+  let result = 0;
+  arr.forEach(num=> result = result + num)
+  return result;
+}
+function partTwo(gamesArr: string[]){
+  const powersOfGameBalls: number[] = []
+  gamesArr.forEach(game=>{
+      const gameSets = getGameSets(game);
+      const minimumColors = getMinimumBallNum(gameSets)
+      powersOfGameBalls.push(getPowerOfBalls(minimumColors))
+  })
+
+  console.log("Part Two Answer: ",getSumOfArrNum(powersOfGameBalls))
+}
+
+
+function partOne(gamesArr: string[]){
   const gameIdsArr: number[] = []
 
   let totalSuccessIDs: number = 0;
@@ -81,7 +145,19 @@ async function main(){
     }
   })
  
-  console.log(totalSuccessIDs);
+  console.log("Part One Answer: ",totalSuccessIDs);
+}
+
+async function main(){
+  const path = './input.txt'
+  const file = Bun.file(path);
+  
+  const text = await file.text();
+  
+  const gamesArr = text.split('\n');
+
+  partOne(gamesArr);
+  partTwo(gamesArr);
 }
 
 
