b153ec530009219b3e1d28f6703ac08ac2177af9
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2023-12-23 21:11:54 +0200

    Day 3 updated

--- day-3/index.ts
+++ day-3/index.ts
@@ -20,10 +20,13 @@ Of course, the actual engine schematic is much larger. What is the sum of all of
 
 */
 
+import { rejects } from "assert";
+import { resolve } from "bun";
+
 
 /*
-  1. Get input and read 
-  2. We need to split the total text into an array of string lines./
+  1. Get input and read X
+  2. We need to split the total text into an array of string lines. X
   3. We find the locations of all the numbers separated by non numbers
     3.1 We need to ensure to get the length of the numbers with each character location being recorded
   4. We then need to find the locations of all the legal symbols excluding "."
@@ -32,38 +35,90 @@ Of course, the actual engine schematic is much larger. What is the sum of all of
   7. Once this step is concluded, all the numbers in the part numbers array must be summed together.
 */
 
-
-const path = './input.txt'
-const file = Bun.file(path);
-
-const text = await file.text();
-const arrayOfPartNumbers = [];
-
-const symbolRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/;
-
-
+type ArrType = {
+  element: string
+  index: number
+  lineNumber: number
+}
 function isStringNumeric(input: string): boolean {
   return !isNaN(Number(input));
 }
 
+async function getSpecialCharAndNumsArrays(lineArr: string[]){
+  let lineNumber: number = 0;
+  
+  return new Promise(async( resolve ) => {
+    await setTimeout(()=> { 
+      lineArr.forEach((element) => {
+        const charArr = element.split("");
+        charArr.forEach((el, i) => {
+            if(symbolRegex.test(el)){
+              arrayOfSpecialChars.push({ element: el, index: i, lineNumber})
+            }
+            if(isStringNumeric(el)){
+              arrayOfNumberChars.push({ element: el, index: i, lineNumber})
+            }
+        })
+        lineNumber ++
+    });
+    resolve("The operation completed")
+    }, 2000)
+  })
+}
 
-const split = text.split('\n')
+async function getPartNumbers(specialCharArr: ArrType[], specialNumberChars: ArrType[]): Promise<number[]>{
+  return new Promise((resolve)=>{
+    const partsArr: number[] = [];
 
+    // check if the previous index number is a number on the line, this will be used to create a full number
+    let prevIndex: number | undefined = undefined;
+    let prevLine: number | undefined = undefined;
+    let prevNum: string | undefined = undefined;
 
-const test = split[5].split("")
+    specialNumberChars.forEach((num)=> {
 
-let lastNumIndex: number;
-test.forEach((element, index) => {
-    if(isStringNumeric(element)){
-      lastNumIndex = index
-    }
-    console.log(isStringNumeric(element), index)
-});
-
-
-// split.forEach(element => {
-//   if(symbolRegex.test(element)){
+        prevIndex = num.index  
+        prevLine = num.lineNumber
+        prevNum = num.element
+    })
     
-//     console.log(element.split('.'))
-//   }
-// });
+    // this is just a test to see if the function works as expected
+    partsArr.push(parseInt(specialNumberChars[2].element));
+    resolve(partsArr)
+  })
+}
+const path = './input.txt'
+const file = Bun.file(path);
+
+const text = await file.text();
+
+
+// with special char, index on line and line number
+let arrayOfSpecialChars: ArrType[] = [];
+// with number, index on line and line number
+let arrayOfNumberChars: ArrType[] = [];
+// Array of part numbers
+let arrayOfPartNumbers: number[];
+
+
+// proper test to see if the character has a special character
+const symbolRegex = /[^a-zA-Z0-9.]/;
+
+
+
+
+
+
+
+
+const newLineSplit = text.split('\n')
+
+getSpecialCharAndNumsArrays(newLineSplit).then(()=>{
+  getPartNumbers(arrayOfSpecialChars, arrayOfNumberChars).then((result: number[])=> {
+    arrayOfPartNumbers = result;
+    console.log(result)
+  })
+})
+
+// console.log(arrayOfSpecialChars)
+// console.log(arrayOfNumberChars)
